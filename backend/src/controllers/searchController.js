427d3f43153f6abd84fa7eb0526a44d20d449af2
Monalisa330 <laurameliza26@gmail.com>
2026-02-18 17:50:18 -0500

    bug

--- backend/src/controllers/searchController.js
+++ backend/src/controllers/searchController.js
@@ -1,4 +1,4 @@
-import { searchService } from "../services/searchService.js";
+import { searchService } from "../services/searchService.js"; 
 
 export const searchProduct = async (req, res, next) => {
   try {
